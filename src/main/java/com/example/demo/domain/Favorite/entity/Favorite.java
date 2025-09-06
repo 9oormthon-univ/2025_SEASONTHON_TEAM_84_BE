ac79c20953b2c47a47b4bd973253360a9f71e479
seo426 <sykang0128@gmail.com>
2025-09-07 04:31:22 +0900

    favorit entity

---
+++ src/main/java/com/example/demo/domain/Favorite/entity/Favorite.java
@@ -0,0 +1,48 @@
+package com.example.demo.domain.Favorite.entity;
+
+import com.example.demo.domain.auditing.entity.BaseTimeEntity;
+import com.example.demo.domain.member.entity.Member;
+import com.example.demo.domain.store.entity.Store;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "favorite",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"member_id", "store_id"})
+        },
+        indexes = {
+                @Index(name = "idx_favorite_member_id", columnList = "member_id"),
+                @Index(name = "idx_favorite_store_id", columnList = "store_id")
+        }
+        )
+public class Favorite extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    public static Favorite create(Member member, Store store) {
+        return Favorite.builder()
+                .member(member)
+                .store(store)
+                .build();
+    }
+
+}
